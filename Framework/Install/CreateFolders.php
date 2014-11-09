9e9779302ebb97418948dc7cf3f35d502c745d3e
mirobrando <mirobrando@wp.pl>
2014-11-09 16:44:32 +0100

    default folders

--- Framework/Install/CreateFolders.php
+++ Framework/Install/CreateFolders.php
@@ -7,6 +7,23 @@ class CreateFolders
 {
     public static function execute()
     {
-        echo "getcwd: " . var_dump(getcwd());
+        $folder = getcwd();
+
+        mkdir($folder . '/common/cache', 777);
+        mkdir($folder . '/common/resources', 755);
+        mkdir($folder . '/common/resources/css', 755);
+        mkdir($folder . '/common/resources/img', 755);
+        mkdir($folder . '/common/resources/js', 755);
+        mkdir($folder . '/common/resources/ng-views', 755);
+
+        mkdir($folder . '/modules', 755);
+
+        mkdir($folder . '/public/css', 755);
+        mkdir($folder . '/public/img', 755);
+        mkdir($folder . '/public/js', 755);
+        mkdir($folder . '/public/views', 755);
     }
+
+
+
 } 
